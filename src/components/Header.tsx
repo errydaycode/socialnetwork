e0ca01d466bd4e3f4efa52fdfe19673562448690
listopadgeorgii <listopadiogl@gmail.com>
2023-02-18 22:01:23 +0300

    01-13

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://cdn-icons-png.flaticon.com/512/59/59439.png" alt="logo"/>
+        </header>
+    );
+};
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://www.bacancytechnology.com/blog/wp-content/uploads/2021/03/17-03-2021-Wednesday%E2%80%93-React-Banner.png" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
