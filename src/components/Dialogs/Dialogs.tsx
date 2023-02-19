540b866757f8f7b0db3e7e442fd29b54ddd525e5
listopadgeorgii <listopadiogl@gmail.com>
2023-02-19 16:35:36 +0300

    21 - dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,39 @@ import  s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Georgy
+                </div>
+                <div className={s.dialog}>
+                    Anna
+                </div>
+                <div className={s.dialog}>
+                    Roman
+                </div>
+                <div className={s.dialog}>
+                    Ivan
+                </div>
+                <div className={s.dialog}>
+                    Kate
+                </div>
+                <div className={s.dialog}>
+                    Izzat
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi!
+                </div>
+                <div className={s.message}>
+                    How is your React?
+                </div>
+                <div className={s.message}>
+                    Awesome!
+                </div>
+            </div>
         </div>
     );
 };
