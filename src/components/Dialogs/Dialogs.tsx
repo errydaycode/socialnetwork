37ac2ebd6b9b4cc5d0e9ebda7308e7ff0d0adc34
listopadgeorgii <listopadiogl@gmail.com>
2023-03-12 16:52:38 +0300

    24 (UI - BLL)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -23,22 +23,47 @@ type MessagePropsType={
 const Message = (props: MessagePropsType) => {
     return <div className={s.message}>{props.message}</div>
 }
+type dialogsDataType ={
+    id: number
+    name: string
+}
+type messagesDataType ={
+    id: number
+    message: string
+}
 
 export const Dialogs = () => {
+
+    let dialogsData: dialogsDataType[] = [
+        {id: 1, name: 'Georgy'},
+        {id: 2, name: 'Anna'},
+        {id: 3, name: 'Ivan'},
+        {id: 4, name: 'Liza'},
+        {id: 5, name: 'Marina'},
+        {id: 6, name: 'Roman'}
+    ]
+
+    let messagesData: messagesDataType[] = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your React?'},
+        {id: 3, message: 'Awesome!'},
+        {id: 4, message: 'Yo!'},
+        {id: 5, message: 'Russia!'}
+    ]
+
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={'Georgy'} id={1}/>
-                <DialogItem name={'Anna'} id={2}/>
-                <DialogItem name={'Ivan'} id={3}/>
-                <DialogItem name={'Liza'} id={4}/>
-                <DialogItem name={'Marina'} id={5}/>
-                <DialogItem name={'Roman'} id={6}/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
             </div>
             <div className={s.messages}>
-                <Message message={'Hi'}/>
-                <Message message={'How is your React?'}/>
-                <Message message={'Awesome!i'}/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
+
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Myposts.tsx
+++ src/components/Profile/MyPosts/Myposts.tsx
@@ -3,7 +3,20 @@ import  s from './Myposts.module.css'
 import {Post} from "./Post/Post";
 
 
+type postsDataType= {
+    id: number,
+    message: string,
+    likesCount: number
+}
+
 const Myposts = () => {
+
+    let postsData: postsDataType[] = [
+        {id: 1, message: '"Yo, I\'m God of Death"', likesCount: 172},
+        {id: 2, message: 'Wassup. Have you lost your Death Note?', likesCount: 172},
+
+    ]
+
     return (
         <div className={s.postsBlock}>
            <h3>My posts</h3>
@@ -12,12 +25,12 @@ const Myposts = () => {
                <div> <button>Add post</button></div>
             </div>
             <div className={s.posts}>
-              <Post message={"Yo, I'm God of Death"}
-                    likesCount={234}
+              <Post message={postsData[0].message}
+                    likesCount={postsData[0].likesCount}
               />
-              <Post message={"Wassup. Have you lost your Death Note? "}
-                    likesCount={172}
-              />
+                <Post message={postsData[1].message}
+                      likesCount={postsData[1].likesCount}
+                />
             </div>
         </div>
     );
