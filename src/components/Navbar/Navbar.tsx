2284e3332ee8c7b1cb71cc8c92680d87bf37a9a8
listopadgeorgii <listopadiogl@gmail.com>
2024-01-14 23:21:14 +0300

    53 - React JS  (extends React.Component)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -29,6 +29,9 @@ export const Navbar = (props: NavbarPropsType) => {
                     <div className={`${s.item} ${s.active}`}>
                         <NavLink to={'/dialogs'} activeClassName={s.active}>Messages</NavLink>
                     </div>
+                      <div className={`${s.item} ${s.active}`}>
+                        <NavLink to={'/users'} activeClassName={s.active}>Users</NavLink>
+                    </div>
                     <div className={s.item}>
                         <NavLink to={'/news'} activeClassName={s.active}>News</NavLink>
                     </div>
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,34 +5,32 @@ import axios from "axios";
 import userPhoto from '../../assets/images/user.png'
 
 
-export const Users = (props: UsersPageType) => {
-    debugger
-    if(props.users.length === 0) {
+export class Users extends React.Component<UsersPageType, any>{
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then((res)=> {
-                props.setUsers(res.data.items)
-            })
+    constructor(props: UsersPageType) {
+        super(props);
 
-        // props.setUsers([
-        //     {id: '1', photoUrl: 'https://sun9-63.userapi.com/s/v1/ig2/r1j_72RzYoXIwur8oBmU3c_MiaOTHNOEQc-l-nFmA4s9mBJz4qw5qtY5fNZXuqpyNuwATdEREh24EsjprZSOyFxr.jpg?size=200x200&quality=96&crop=137,41,325,325&ava=1', isFollowed: true, fullName: 'Georgy', status: 'I am front-end dev', location: {city: 'Moscow', country: 'Russia'} },
-        //     {id: '2', photoUrl: 'https://sun9-63.userapi.com/s/v1/ig2/r1j_72RzYoXIwur8oBmU3c_MiaOTHNOEQc-l-nFmA4s9mBJz4qw5qtY5fNZXuqpyNuwATdEREh24EsjprZSOyFxr.jpg?size=200x200&quality=96&crop=137,41,325,325&ava=1', isFollowed: false, fullName: 'Ivan', status: 'I am back-end dev', location: {city: 'Minsk', country: 'Belarus'} },
-        //     {id: '3', photoUrl: 'https://sun9-63.userapi.com/s/v1/ig2/r1j_72RzYoXIwur8oBmU3c_MiaOTHNOEQc-l-nFmA4s9mBJz4qw5qtY5fNZXuqpyNuwATdEREh24EsjprZSOyFxr.jpg?size=200x200&quality=96&crop=137,41,325,325&ava=1', isFollowed: false, fullName: 'Anna', status: 'I am a designer', location: {city: 'Toronto', country: 'Canada'} },
-        // ])
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then((res) => {
+                    this.props.setUsers(res.data.items)
+                })
+
     }
 
-    return (
-        <div>
+
+    render() {
+        return  <div>
+
             {
-                props.users.map(u => <div key={u.id}>
+                this.props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
                             <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="userAvatar" className={s.userPhoto}/>
                         </div>
                         <div>
                             {u.followed ?
-                                <button onClick={()=> props.unFollow(u.id)}>UnFollow</button>
-                                : < button onClick={()=> props.follow(u.id)}>Follow</button> }
+                                <button onClick={()=> this.props.unFollow(u.id)}>UnFollow</button>
+                                : < button onClick={()=> this.props.follow(u.id)}>Follow</button> }
                         </div>
                     </span>
                     <span>
@@ -48,5 +46,6 @@ export const Users = (props: UsersPageType) => {
                 </div>)
             }
         </div>
-    );
-};
+    }
+}
+
